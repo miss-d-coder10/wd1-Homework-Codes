e927187f5b53543c6bd847daed31eba245ccda99
miss-coder10 <dara.dc10@gmail.com>
2016-10-02 23:17:37 +0100

    tic tac toe game long code

---
+++ miss-d-coder10/w01d05/tic-tac-toe/js/app.js
@@ -0,0 +1,59 @@
+console.log("tic-tac-toe");
+
+var boxes = document.querySelectorAll(".box");
+var clickBox = 0;
+
+for(var i=0;i<boxes.length;i++){
+  //console.log(boxes[i]);
+  boxes[i].addEventListener("click", function(){
+    if(this.textContent === ""){
+      clickBox++;
+      this.textContent = clickBox%2 === 0 ? "O" : "X";
+    }
+
+
+
+//winning conditions
+if(boxes[0].textContent && boxes[0].textContent === boxes[1].textContent && boxes[0].textContent === boxes[2].textContent) {
+      alert("boom we have a winner");    //row 1
+  } else if (boxes[3].textContent && boxes[3].textContent === boxes[4].textContent && boxes[3].textContent === boxes[5].textContent){
+      alert("boom we have a winner"); // row 2
+  } else if (boxes[6].textContent && boxes[6].textContent === boxes[7].textContent && boxes[6].textContent === boxes[8].textContent){
+      alert("boom we have a winner"); // row 3
+  } else if (boxes[0].textContent && boxes[0].textContent === boxes[3].textContent && boxes[0].textContent === boxes[6].textContent){
+      alert("boom we have a winner"); // column 1
+  } else if (boxes[1].textContent && boxes[1].textContent === boxes[4].textContent && boxes[1].textContent === boxes[7].textContent){
+      alert("boom we have a winner");  // column 2
+  } else if (boxes[2].textContent && boxes[2].textContent === boxes[5].textContent && boxes[2].textContent === boxes[8].textContent){
+      alert("boom we have a winner");  // column 3
+  } else if (boxes[0].textContent && boxes[0].textContent === boxes[4].textContent && boxes[0].textContent === boxes[8].textContent){
+      alert("boom we have a winner");  // diagonal1
+  } else if (boxes[2].textContent && boxes[2].textContent === boxes[4].textContent && boxes[2].textContent === boxes[6].textContent){
+      alert("boom we have a winner");  // diagonal2
+  } else if (clickBox === 9){
+    alert("it's a draw");
+  } else{
+    return;
+  }
+
+
+  //reset the board
+  var resetButton = document.getElementById("resetbutton");
+  resetButton.addEventListener("click", clearTheBoard);
+
+  function clearTheBoard(){
+    clickBox = 0;
+    for(var i=0; i<boxes.length;i++){
+      boxes[i].textContent = "";
+      }
+    }
+
+
+
+
+
+
+
+
+});
+}
