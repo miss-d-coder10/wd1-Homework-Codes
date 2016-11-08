b62aaaa470ae295e669588cb35b9c9913da12534
miss-coder10 <dara.dc10@gmail.com>
2016-11-08 19:15:13 +0000

    initial commit

---
+++ miss-d-coder10/w07d02/tic-tac-toe/js/gameController.js
@@ -0,0 +1,26 @@
+angular.module('TicTacToe')
+  .controller('GameController', GameController);
+
+function GameController() {
+  this.clickBox = 0;
+  this.cells = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
+  ];
+
+  function mrClick($index) {
+    this.clickBox++;
+    if(this.clickBox%2){
+      this.cells[$index] = 'X';
+    }else
+      this.cells[$index] = '0';
+  }
+  this.mrClick = mrClick;
+}
